f7178bd0a84ac2270b81d69b25d686cc90a4de99
daliyajohn75@gmail.com <daliyajohn75@gmail.com>
2020-07-31 22:39:10 +0530

    feat(core): add carousel library

---
+++ projects/carousel/src/lib/carousel.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-carousel',
+  template: `
+    <p>
+      carousel works!
+    </p>
+  `,
+  styles: []
+})
+export class CarouselComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
