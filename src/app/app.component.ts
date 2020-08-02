c74d309fc7bb5c37563937d805806e8b64785b16
daliyajohn75@gmail.com <daliyajohn75@gmail.com>
2020-08-02 23:20:39 +0530

    feat(core): pass data from app to library

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  slides: any;
+  constructor() {
+    this.slides = [
+      {
+        'image' : 'https://www.publicdomainpictures.net/pictures/150000/velka/banner-header-tapete-1452531737Jr0.jpg',
+        'url': "url",
+        'caption': 'caption'
+      },
+      {
+        'image' : 'https://sweep.ac.uk/wp-content/uploads/yellow-banner.jpg',
+        'url': "url",
+        'caption': 'caption'
+      },
+      {
+        'image' : 'https://inacomp.net/wp-content/uploads/2015/10/banner-blue-background.png',
+        'url': "url",
+        'caption': 'caption'
+      }
+    ];
+  }
 }
