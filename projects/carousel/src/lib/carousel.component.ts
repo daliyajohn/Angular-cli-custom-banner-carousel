807d376e6d1d98184bedcdfe3a9ce2902982353e
daliyajohn75@gmail.com <daliyajohn75@gmail.com>
2020-08-02 23:21:27 +0530

    feat(library): implement slider functionality

--- projects/carousel/src/lib/carousel.component.ts
+++ projects/carousel/src/lib/carousel.component.ts
@@ -1,23 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild, Input } from '@angular/core';
+import { trigger, transition, animate, style } from '@angular/animations';
 
 @Component({
   selector: 'lib-carousel',
   templateUrl: './carousel.component.html',
-  styleUrls: ['./carousel.component.scss']
+  styleUrls: ['./carousel.component.scss'],
+  animations: [
+    trigger('carouselAnimation', [
+      transition('void => *', [
+        style({ opacity: 0 }),
+        animate('300ms', style({ opacity: 1 }))
+      ]),
+      transition('* => void', [
+        animate('300ms', style({ opacity: 0 }))
+      ])
+    ])
+  ]
 })
 export class CarouselComponent implements OnInit {
-
-  constructor() { }
+  @Input() slides;
+  currentSlide = 0;
+  constructor() {
+  }
 
   ngOnInit() {
+    this.preloadImages();
   }
 
-  next() {
-    //code
+  onPreviousClick() {
+    const previous = this.currentSlide - 1;
+    this.currentSlide = previous < 0 ? this.slides.length - 1 : previous;
   }
 
-  preview() {
+  onNextClick() {
+    const next = this.currentSlide + 1;
+    this.currentSlide = next === this.slides.length ? 0 : next;
+  }
 
+  preloadImages() {
+    for (const slide of this.slides) {
+      new Image().src = slide.image;
+    }
   }
 
 }
--- projects/carousel/src/lib/carousel.module.ts
+++ projects/carousel/src/lib/carousel.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { CarouselComponent } from './carousel.component';
 
 @NgModule({
   declarations: [CarouselComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   exports: [CarouselComponent]
 })
